5f7187b40fc06d5dbe77fe2c2fd184f0f1cb0ca6
sakh-ML <ml.abusaleh.ml@gmail.com>
2025-05-28 12:55:48 +0200

    was added

---
+++ from_python_toC_assembly.py
@@ -0,0 +1,28 @@
+asm = r'''"mov $0xFFFFFFFFFFFFFFFF, %%rax;"
+"mov $0xFFFFFFFFFFFFFFFE, %%r8;"
+"xor %%rdx, %%rdx;"
+'''
+
+for _ in range(1000):
+    asm += '"div %%r8;"\n'
+    for _ in range(5):
+        for register in range(9, 16):
+            # CHANGE CODE HERE
+            src_register = ''
+            asm += f'"add %%r{register}, {src_register};"' + '\n'
+
+program = rf"""#define REPS 1000000UL
+
+int main() {{
+    for (unsigned long i = 0; i < REPS; i++) {{
+        asm volatile (
+{asm}:::"r8","r9","r10","r11","r12","r13","r14","r15"
+        );
+    }}
+}}
+
+"""
+
+with open("bench.c", "w") as f:
+    f.write(program)
+
