4344f32dffb920aad405ed5992d640f2922084cb
sakh-ML <ml.abusaleh.ml@gmail.com>
2025-05-29 16:39:21 +0200

    a comment was added

--- c/system/for_parallel_omp.c
+++ c/system/for_parallel_omp.c
@@ -1,23 +1,22 @@
 #include <math.h>
-  1 #include <stdio.h>
-  2 #include <omp.h>
-  3 
-  4 #include "myFunction.h"
-  5 
-  6 int main(){
-  7 
-  8     int size = 1000000, i;
-  9     double start = omp_get_wtime();
- 10     double myArray[size];
- 11     #pragma omp parallel for
- 12         for(int i = 0; i < size; ++i){
- 13             myArray[i] = myFunc(i);
- 14         }
- 15         printf("\n<<< Fertig >>>\n");
- 16     
- 17     
- 18     double end = omp_get_wtime();
- 19     printf("The time needen : %d ns", end - start);
- 20         return 0;
- 21 }
+#include <stdio.h>
+#include <omp.h>
+#include "myFunction.h"
+   
+int main(){
+
+	int size = 1000000, i; 
+        double start = omp_get_wtime();
+        double myArray[size];
+        #pragma omp parallel for
+          for(int i = 0; i < size; ++i){
+              myArray[i] = myFunc(i);
+          }
+        printf("\n<<< Fertig >>>\n");
+      
+        double end = omp_get_wtime();
+        printf("The time needen : %d ns", end - start);
+
+        return 0;
+}
 
