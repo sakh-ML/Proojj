0c74215c33203d7dc2fac3114d60ebe8a96f6c00
sakh-ML <ml.abusaleh.ml@gmail.com>
2025-05-29 16:10:40 +0200

    changed ..

---
+++ binar.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class Node{
+	
+public:
+	Node* left;
+	int value;
+	Node* right;
+
+	Node(int v) : value{v}, left{nullptr}, right{nullptr}{}
+};
+
+class BinBaum{};
+
+
+
+int main(){
+
+}
