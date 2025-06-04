9156261327e2617b05b0e89d568968f96e24e37d
sakh-ML <ml.abusaleh.ml@gmail.com>
2025-06-04 12:15:29 +0200

    new file was added

---
+++ branchpredicition.c
@@ -0,0 +1,32 @@
+// compile with gcc -O0 bench.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TEST_SIZE 10000000
+
+unsigned long test_data[TEST_SIZE];
+
+double bench(unsigned long ratio) {
+    // Generate random data
+    for (unsigned long i = 0; i < TEST_SIZE; i++) {
+        test_data[i] = rand() % 100;
+    }
+
+    volatile unsigned long x;
+    clock_t before = clock();
+    for (unsigned long i = 0; i < TEST_SIZE; i++) {
+        // INSERT CODE HERE
+    }
+    clock_t after = clock();
+
+    return ((double)(after - before)) / CLOCKS_PER_SEC;
+}
+
+int main() {
+    for (unsigned long i = 0; i <= 100; i++) {
+        double time = bench(i);
+        printf("%d, %f\n", i, time);
+    }
+}
+
