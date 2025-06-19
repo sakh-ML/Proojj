a8cf3382a24e031920ab30526fc3b1a468cbcbea
sakh-ML <ml.abusaleh.ml@gmail.com>
2025-06-19 20:36:22 +0200

    a typpo was fixed

--- networking/networking.cpp
+++ networking/networking.cpp
@@ -1,9 +1,10 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
+
 int main(){
 
-int new_socket = socket(AF_NET, SOCK_STREAM, 0);
+int new_socket = socket(AF_INET, SOCK_STREAM, 0);
 
 
 	return 0;
