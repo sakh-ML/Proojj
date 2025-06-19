e1e27d51736b3deb83101345010ac5b1d00862bc
sakh-ML <ml.abusaleh.ml@gmail.com>
2025-06-19 20:48:36 +0200

    a little was added

--- networking/networking.cpp
+++ networking/networking.cpp
@@ -4,8 +4,15 @@
 
 int main(){
 
-int new_socket = socket(AF_INET, SOCK_STREAM, 0);
+int my_socket = socket(AF_INET, SOCK_STREAM, 0);
 
+if(my_socket == -1){
+	return -1;
+}
+struct sockadrr_in my_addr;
+memset(my_addr.sa_data, );
+
+if(bind(my_socket, ))
 
 	return 0;
 }
