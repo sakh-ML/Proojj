1809ad52a5ab71d5342bc1b60d81e7078eba9cde
sakh-ML <ml.abusaleh.ml@gmail.com>
2025-06-28 15:26:54 +0200

    the cache function was added, seqeutnill,parallel access for the data in the cache !

---
+++ headers/cache_benchmark.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+#include "cacheline.h"
+#include "helper.h"
+
+#define ELEMENTS 50000000
+
+int main(int argc, char **argv) {
+    Timer timer;
+    std::vector<uint64_t> indices(ELEMENTS);
+    cacheLine *data_array = new cacheLine[ELEMENTS];
+    uint64_t result = 0;
+
+    // Erzeuge Daten für den Benchmark und fülle indices mit aufsteigenden Werten
+    prepare_benchmark(data_array, indices, ELEMENTS);
+
+    // Starte den sequentiellen Durchlauf
+    timer.start();
+    for (auto i = 0U; i < ELEMENTS; ++i) {
+        result += data_array[indices[i]].compute();
+    }
+    timer.stop();
+    std::cout << "Die Ausführung mit sequentiellen Zugriffen hat " << timer.elapsedSeconds() << " Sekunden benötigt (check: " << result << ")!" << std::endl;
+
+    // Shuffle nun die Indizes, damit die Daten im Datenarray nun zufällig zugegriffen werden
+    shuffle_indices(indices);
+    result = 0;
+    timer.start();
+    for (auto i = 0U; i < ELEMENTS; ++i) {
+        result += data_array[indices[i]].compute();
+    }
+    timer.stop();
+    std::cout << "Die Ausführung mit zufälligen Zugriffen hat " << timer.elapsedSeconds() << " Sekunden benötigt (check: " << result << ")!" << std::endl;
+
+    // Füge nun Prefetch-Instruktionen hinzu
+    result = 0;
+    auto prefetch_distance = 0U;
+    timer.start();
+
+    for(auto i = 0U; i < ELEMENTS; ++i){
+        
+    }
+
+    timer.stop();
+    std::cout << "Die Ausführung mit zufälligen Zugriffen und SW-Prefetching hat " << timer.elapsedSeconds() << " Sekunden benötigt (check: " << result << ")!" << std::endl;
+}
