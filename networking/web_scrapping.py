e5e74a31c9c3d6a826e25b1aea1bf271b1e0d242
sakh-ML <ml.abusaleh.ml@gmail.com>
2025-07-16 14:13:37 +0200

    Removed .venv from tracking and added to .gitignore

--- networking/web_scrapping.py
+++ networking/web_scrapping.py
@@ -1,1 +1,9 @@
-print("hello")
+import requests
+URL = "https://en.wikipedia.org/wiki/List_of_highest-grossing_films"
+
+respone = requests.get(URL)
+
+html_web_page_code = respone.text
+print(html_web_page_code)
+
+
