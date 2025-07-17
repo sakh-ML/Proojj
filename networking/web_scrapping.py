9e43c1486545dba9163eb2962c35b69c87f26216
sakh-ML <ml.abusaleh.ml@gmail.com>
2025-07-17 14:42:25 +0200

    Delete networking/web_scrapping.py

--- networking/web_scrapping.py
+++
@@ -1,9 +0,0 @@
-import requests
-URL = "https://en.wikipedia.org/wiki/List_of_highest-grossing_films"
-
-respone = requests.get(URL)
-
-html_web_page_code = respone.text
-print(html_web_page_code)
-
-
