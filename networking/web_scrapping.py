682310f725923cf806aca5e0868840dcad38982c
sakh-ML <ml.abusaleh.ml@gmail.com>
2025-07-19 01:52:20 +0200

    web_scrapping.py was modifed

--- networking/web_scrapping.py
+++ networking/web_scrapping.py
@@ -16,14 +16,25 @@ headers = [line.strip() for line in headers.split('\n') if line.strip()]
 df = pd.DataFrame(columns=headers)
 
 
-rows = table.find_all('tr')
+rows = table.find_all('tr')[1:]
 
-arr_str_arr = []
-for row in rows[1 : ]:
+#arr_str_arr = []
+
+for row in rows:
     cells = row.find_all(['th' , 'td'])
-    arr_str_arr.append([element.get_text().strip() for element in cells])
-    df.loc[len(df)] = [element.get_text().strip() for element in cells]
+    clean_row = []
+    #delete all tje sups
+    for cell in cells:
+        for sup in cell.find_all('sup'):
+            sup.decompose()
+        clean_row.append(cell.get_text(strip=True))
+    df.loc[len(df)] = clean_row
+        
+    #print(cells)
+    #arr_str_arr.append([element.get_text().strip() for element in cells])
+    #df.loc[len(df)] = [element.get_text().strip() for element in cells]
 
 
 #print(df.to_string(index=False))
 
+print(df.to_string(index=False))
