46580bc0e98989ab43c5a0b90005ad8bc5ce6185
sakh-ML <ml.abusaleh.ml@gmail.com>
2025-07-18 23:12:34 +0200

    web_scrapping was modifed

--- networking/web_scrapping.py
+++ networking/web_scrapping.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+import pandas as pd
+
 URL = 'https://en.wikipedia.org/wiki/List_of_highest-grossing_films'
 
 response = requests.get(URL)
@@ -11,5 +13,6 @@ table_2 = soup.find('table', class_ = "wikitable plainrowheaders")
 colunms = table_2.find('tr').text
 colunms = [line.strip() for line in colunms.split('\n') if line.strip()]
 
-print(type(colunms))
-print(colunms)
+
+df = pd.DataFrame(columns=colunms)
+
