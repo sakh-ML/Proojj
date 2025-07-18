372d53e7544d2a4d1463c8a33cfc2f47e3b5caf0
sakh-ML <ml.abusaleh.ml@gmail.com>
2025-07-18 05:20:46 +0200

    ss

---
+++ networking/web_scrapping.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://en.wikipedia.org/wiki/List_of_highest-grossing_films"
+
+respone = requests.get(URL)
+
+html_web_page_code = respone.text
+print(html_web_page_code)
+
+
