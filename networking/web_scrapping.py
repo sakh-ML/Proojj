f9b473248b62741f3f44c5ddce4cd5c12c628b3a
sakh-ML <ml.abusaleh.ml@gmail.com>
2025-07-18 05:23:02 +0200

    web scrapping was modfied

--- networking/web_scrapping.py
+++ networking/web_scrapping.py
@@ -1,11 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://en.wikipedia.org/wiki/List_of_highest-grossing_films"
+URL = 'https://en.wikipedia.org/wiki/List_of_highest-grossing_films'
 
-respone = requests.get(URL)
+response = requests.get(URL)
+soup = BeautifulSoup(response.content, 'html.parser')
 
-html_web_page_code = respone.text
-print(html_web_page_code)
+table_2 = soup.find('table', class_ = "wikitable plainrowheaders")
 
+colunms = table_2.find('tr').text
+colunms = [line.strip() for line in colunms.split('\n') if line.strip()]
 
+print(type(colunms))
+print(colunms)
