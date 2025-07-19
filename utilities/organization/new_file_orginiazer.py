2d4f49c7185e8138ce86cb776de56843c8da2ed5
sakh-ML <ml.abusaleh.ml@gmail.com>
2025-07-19 14:44:16 +0200

    new_file_orginiazer was modifed. still a lot of logic erros but ...

--- utilities/organization/new_file_orginiazer.py
+++ utilities/organization/new_file_orginiazer.py
@@ -2,8 +2,8 @@ import os
 import shutil
 
 file_types = {
-    "Photos" : ["png","webp"],
-    "Files" : ["pdf"]
+    "Photos" : ["png","webp", "jpeg"],
+    "Docu" : ["pdf"]
     #etc .....
 }
 
@@ -19,17 +19,45 @@ for file in os.listdir(PATH):
     _, folder_extenction = os.path.splitext(file)
     if folder_extenction == "":
         continue
+    print(file)
     folder_extenction = folder_extenction[1:]
+    print(folder_extenction)
     
     for key, array_value in file_types.items():
         #the extcteion of the file, is in our files_types
         if folder_extenction in array_value:
-            print("Pingo, add it to the right key!!")
-        #the excteion of this file is not found in the files_types. add it to a folder other (platzhalter)
-        else:
-            print("!!!, lets add it for now to the other folder")
+            
+            #erstelle ein Folder Path/key wenn es nicht exist und file da moven.
+            where_to_move_the_file = f"{PATH}/{key}/{file}" 
+            full_file_path = f"{PATH}/{file}"
+            
+            if os.path.exists(where_to_move_the_file):
+                shutil.move(full_file_path, where_to_move_the_file)
+                break
+            
+            #erstelle where_to_move_the_file if not exists, und dann file da bewegen
+            else:
+                #to fix
+                os.mkdir(where_to_move_the_file)
+                shutil.move(full_file_path, where_to_move_the_file)
+                break
+                
         
-    #if :
-    #    asdasd
-    #else:
-           
+    #the excteion of this file is not found in the files_types. add it to a folder other (platzhalter)
+    #erstelle ein folder Others wenn es nicht existiert, und da file bewegen
+    else:
+        
+        new_directry_path = f"{PATH}/Others"
+        where_to_move_file = f"{PATH}/Others/{file}"
+        where_file_was = f"{PATH}/{file}" 
+        
+        if os.path.exists(new_directry_path):
+            
+            shutil.move(where_file_was, where_to_move_file)
+            break
+        else:
+            os.mkdir(new_directry_path)
+            shutil.move(where_file_was, where_to_move_file)
+            
+    
+                  
