c831d8e034fef969596e60a5f8768325fd14c23b
sakh-ML <ml.abusaleh.ml@gmail.com>
2025-09-07 19:26:58 +0200

    using renderer

--- algorithms/fluid.c
+++ algorithms/fluid.c
@@ -21,28 +21,25 @@ typedef struct Particle{
 } Particle;
 
 
-void drawCircle(SDL_Surface* surface, int cx, int cy, int radius, Uint32 color) {
-    for (int y = -radius; y <= radius; y++) {
-        for (int x = -radius; x <= radius; x++) {
+void drawCircle(SDL_Renderer* renderer, int cx, int cy, int radius) {
+    for (int x = -radius; x <= radius; x++) {
+        for (int y = -radius; y <= radius; y++) {
             if (x*x + y*y <= radius*radius) { // inside circle
-                int px = cx + x;
-                int py = cy + y;
-                if (px >= 0 && px < surface->w && py >= 0 && py < surface->h) {
-                    Uint32* pixels = (Uint32*)surface->pixels;
-                    pixels[py * surface->w + px] = color;
-                }
+				SDL_RenderDrawPoint(renderer, cx + x, cy + y);
+     
             }
         }
     }
 }
 
 
-void drawParticles(SDL_Surface* surface,Particle particles[]){
+void drawParticles(SDL_Renderer* renderer,Particle particles[]){
 
-    Uint32 blue = SDL_MapRGB(surface->format, 0, 0, 255);
+	//blue color
+	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255); 
    
     for(int i = 0; i < particle_count; ++i){
-        drawCircle(surface, particles[i].x, particles[i].y, particles[i].radius, blue);
+        drawCircle(renderer, particles[i].x, particles[i].y, particles[i].radius);
     }
 }
 
@@ -83,18 +80,20 @@ void handleCollisions(Particle particles[]){
 		}
 	}
 
+	//ground collison
 	for(int i = 0; i < particle_count; ++i){
-		if(particles[i].y + particles[i].radius >= SCREEN_HEIGHT){
-			particles[i].y -= particles[i].vy;
+		if(particles[i].y + particles[i].radius >= SCREEN_HEIGHT || particles[i].y - particles[i].radius <= 0){
+			particles[i].vy *= -0.9;
 		}
 	}
+
 }
 
 
 int main(){
 
-	SDL_Surface* screenSurface = NULL;
 	SDL_Window* window = NULL;
+	SDL_Renderer* renderer = NULL;
 
 	if(SDL_Init(SDL_INIT_VIDEO) == -1){
 		printf("Could not initialize SDL: %s.\n", SDL_GetError());
@@ -108,43 +107,28 @@ int main(){
         exit(1);
 	}
 	
-	screenSurface = SDL_GetWindowSurface(window);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-	//velcoity for now is 0
-	Particle particle = {SCREEN_HEIGHT / 2, SCREEN_WIDTH / 2, 0, 0.5, 20};
     Particle particels[MAX_PARTICLES];
-    Uint32 blue = SDL_MapRGB(screenSurface->format, 0, 0, 255);
-    Uint32 black = SDL_MapRGB(screenSurface->format, 0, 0, 0);
-   
-	
+	memset(particels, 0, sizeof(particels));
+
 	bool running = true;
+
 	while(running){
 		
-		SDL_FillRect(screenSurface, NULL, black);
-		drawParticles(screenSurface, particels);
+		//clear background to black
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); 
+		SDL_RenderClear(renderer);
 
-		SDL_UpdateWindowSurface(window);
+		// draw all particles on the screen
+		drawParticles(renderer, particels);
+		SDL_RenderPresent(renderer);
+
+		//SDL_UpdateWindowSurface(window);
 		SDL_Event event;
 		while(SDL_PollEvent(&event)){
 			if(event.type == SDL_QUIT){
 				running = false;
-			}
-			else if(event.type == SDL_KEYDOWN){
-
-				if(event.key.keysym.sym == SDLK_DOWN){
-					particle.y += 5;
-	
-				}
-				else if(event.key.keysym.sym == SDLK_UP){
-					particle.y -= 5;
-	
-				}
-				else if(event.key.keysym.sym == SDLK_LEFT){
-					particle.x -= 5;
-				}
-				else if(event.key.keysym.sym == SDLK_RIGHT){
-					particle.x += 5;
-				}
 			}
 
 			else if(event.type == SDL_MOUSEBUTTONDOWN){
@@ -174,6 +158,7 @@ int main(){
 
 		addVelcoityToParticles(particels);
 		handleCollisions(particels);
+		SDL_Delay(5); 
 	}
 
 	return 0;
